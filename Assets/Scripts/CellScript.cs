d8246bf235d4df224c728449c877a1d5a7e9eb2f
Pranabesh <pranabesh.sinha@gmail.com>
2015-03-22 01:21:43 -0700

    Added walls as enums

--- Assets/Scripts/CellScript.cs
+++ Assets/Scripts/CellScript.cs
@@ -3,8 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class CellScript : MonoBehaviour {
-	public List<Transform> Adjacents;
+	public List<CellNode> Adjacents;
 	public Vector3 Position;
 	public int Weight;
 	public bool seen = false;
+	public enum Walls { NORTH, SOUTH, EAST, WEST };
+
+
+	public CellScript()
+	{
+		Adjacents = new List<CellNode> ();
+	}
+
+	public class CellNode
+	{
+		public Transform cell;
+		public Walls parentWrtChild;
+		//hack to see the enum value
+		public int debugView_Walls {get {return (int) parentWrtChild; }}
+
+		public CellNode(Transform _cell, Walls _parentWrtChild)
+		{
+			cell = _cell;
+			parentWrtChild = _parentWrtChild;
+		
+		}
+	}
+
 }
--- Assets/Scripts/GridScript.cs
+++ Assets/Scripts/GridScript.cs
@@ -3,17 +3,22 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class EdgeNode :  IComparable<EdgeNode>{
+public class EdgeNode :  IComparable<EdgeNode>
+{
 	public Transform thisEdge;
 	public int edgeWeight;
-	public List<Transform> Adjacents;
+	public List<EdgeNode> Adjacents;
 	public EdgeNode parent;
+	public Walls childWrtParent;
 
-	public EdgeNode(Transform edge, int weight, EdgeNode forfather){
+	public enum Walls { NORTH, SOUTH, EAST, WEST };
+
+	public EdgeNode(Transform edge, int weight, EdgeNode forefather)
+	{
 		thisEdge = edge;
 		edgeWeight = weight;
-		parent = forfather;
-		Adjacents = new List<Transform> ();
+		parent = forefather;
+		Adjacents = new List<EdgeNode> ();
 	}
 
 	public int CompareTo(EdgeNode right)
@@ -22,7 +27,8 @@ public class EdgeNode :  IComparable<EdgeNode>{
 	}
 }
 
-public class GridScript : MonoBehaviour {
+public class GridScript : MonoBehaviour 
+{
 	public Transform CellPrefab;
 	public Vector3 GridSize;
 	public Transform[,] Grid;
@@ -34,8 +40,6 @@ public class GridScript : MonoBehaviour {
 	//The weight can't be the key since there will be duplicates
 	private List<EdgeNode> Heap;
 
-	private EdgeNode ed;
-
 	void Start()
 	{
 		CreateGrid ();
@@ -46,40 +50,47 @@ public class GridScript : MonoBehaviour {
 		Prim ();
 	}
 
-	void Prim(){
+	void Prim()
+	{
 		//Lets just assume Start cell is Grid[0,0]
 		Transform cell = Grid[0,0];
 		//Grid[0,0].GetComponent<Renderer>().material.color = Color.green;
 		EdgeNode start = new EdgeNode (cell, 0, null);//no edge weight for start cell
 		start.thisEdge.GetComponent<CellScript>().seen = true;
 		Maze.Add (start);
-		foreach (Transform adj in cell.GetComponent<CellScript>().Adjacents) {
+		foreach (CellScript.CellNode adj in cell.GetComponent<CellScript>().Adjacents)
+		{
 			int parentWeight   = cell.GetComponent<CellScript> ().Weight; 
-			int adjacentWeight = adj.GetComponent<CellScript> ().Weight;
-			Heap.Add (new EdgeNode (adj, Mathf.Abs (parentWeight - adjacentWeight), start));
+			int adjacentWeight = adj.cell.GetComponent<CellScript> ().Weight;
+			Heap.Add (new EdgeNode (adj.cell, Mathf.Abs (parentWeight - adjacentWeight), start));
 		}
 		Heap.Sort ();
 		//stop when maze contains all the vertices
-		while (Maze.Count < GridSize.x * GridSize.z) {
+		while (Maze.Count < GridSize.x * GridSize.z) 
+		{
 			//get edge with lowest edge weight
 			EdgeNode minNode = Heap[0];
 			Heap.RemoveAt(0);
 			//Add this vertex if it isn't already in the maze
-			if(!minNode.thisEdge.GetComponent<CellScript>().seen){
+			if(!minNode.thisEdge.GetComponent<CellScript>().seen)
+			{
 				minNode.thisEdge.GetComponent<CellScript>().seen = true;
 				Maze.Add(minNode);
-				minNode.parent.Adjacents.Add(minNode.thisEdge);
+				minNode.parent.Adjacents.Add(minNode);
+
 			//	minNode.thisEdge.GetComponent<Renderer>().material.color = Color.red;
 			}
 			else 
 				continue;
 			//Now add the adjacents of this new vertex into the heap
-			foreach (Transform adj in minNode.thisEdge.GetComponent<CellScript>().Adjacents) {
+			foreach (CellScript.CellNode adj in minNode.thisEdge.GetComponent<CellScript>().Adjacents)
+			{
 				//provided it isn't in the maze
-				if(!adj.GetComponent<CellScript>().seen){
+				if(!adj.cell.GetComponent<CellScript>().seen)
+				{
 					int parentWeight   = minNode.thisEdge.GetComponent<CellScript> ().Weight; 
-					int adjacentWeight = adj.GetComponent<CellScript> ().Weight;
-					Heap.Add (new EdgeNode (adj, Mathf.Abs (parentWeight - adjacentWeight), minNode));
+					int adjacentWeight = adj.cell.GetComponent<CellScript> ().Weight;
+					Heap.Add (new EdgeNode (adj.cell, Mathf.Abs (parentWeight - adjacentWeight), minNode));
 				}
 			}
 			Heap.Sort ();
@@ -87,8 +98,22 @@ public class GridScript : MonoBehaviour {
 		PrintMaze ();
 	}
 
-	void PrintMaze(){
-		foreach (EdgeNode ed in Maze) {
+
+	void CreateMaze()
+	{
+		foreach (EdgeNode vertex in Maze)
+		{
+			foreach(EdgeNode adjacent in vertex.Adjacents)
+			{
+
+			}
+		}
+	}
+
+	void PrintMaze()
+	{
+		foreach (EdgeNode ed in Maze) 
+		{
 			Debug.Log(ed.thisEdge.GetComponent<CellScript> ().name);
 		}
 	}
@@ -96,8 +121,10 @@ public class GridScript : MonoBehaviour {
 	void CreateGrid()
 	{
 		Grid = new Transform[(int)GridSize.x,(int)GridSize.z];
-		for(int i = 0; i < GridSize.x; i++){
-			for(int j = 0; j < GridSize.z; j++){
+		for(int i = 0; i < GridSize.x; i++)
+		{
+			for(int j = 0; j < GridSize.z; j++)
+			{
 				Transform newCell;
 				newCell = (Transform)Instantiate (CellPrefab, new Vector3(i, 0, j), Quaternion.identity);
 				newCell.parent = transform;
@@ -110,8 +137,10 @@ public class GridScript : MonoBehaviour {
 		Camera.main.orthographicSize = Mathf.Max (GridSize.x/2, GridSize.z/2);
 	}
 
-	void SetRandomCellWeights(){
-		foreach (Transform child in transform) {
+	void SetRandomCellWeights()
+	{
+		foreach (Transform child in transform)
+		{
 			TextMesh childText = child.GetComponentInChildren<TextMesh>();
 			int weight = UnityEngine.Random.Range(0,10);
 			childText.text = weight.ToString();
@@ -119,31 +148,50 @@ public class GridScript : MonoBehaviour {
 		}
 	}
 
-	void SetAdjacents(){
-		for(int x = 0; x < GridSize.x; x++){
-			for(int z = 0; z < GridSize.z; z++){
+	void SetAdjacents()
+	{
+		for(int x = 0; x < GridSize.x; x++)
+		{
+			for(int z = 0; z < GridSize.z; z++)
+			{
 				Transform cell = Grid[x,z];
 				CellScript cScript = cell.GetComponent<CellScript>();
+
+				CellScript.CellNode cellNode;
 				if(x-1 >= 0)
-					cScript.Adjacents.Add(Grid[x-1,z]);
+				{
+					cellNode = new CellScript.CellNode(Grid[x-1,z], CellScript.Walls.WEST);
+					cScript.Adjacents.Add(cellNode);
+				}
 				if(x+1 < GridSize.x)
-					cScript.Adjacents.Add(Grid[x+1,z]);
+				{
+					cellNode = new CellScript.CellNode(Grid[x+1,z], CellScript.Walls.EAST);
+					cScript.Adjacents.Add(cellNode);
+				}
 				if(z-1 >= 0)
-					cScript.Adjacents.Add(Grid[x,z-1]);
+				{
+					cellNode = new CellScript.CellNode(Grid[x,z-1], CellScript.Walls.SOUTH);
+					cScript.Adjacents.Add(cellNode);
+				}
 				if(z+1 < GridSize.z)
-					cScript.Adjacents.Add(Grid[x,z+1]);
+				{
+					cellNode = new CellScript.CellNode(Grid[x,z+1], CellScript.Walls.NORTH);
+					cScript.Adjacents.Add(cellNode);
+				}
 				cScript.Adjacents.Sort(SortAdjacentsByWeight);
 			}
 		}
 	}
 
-	int SortAdjacentsByWeight(Transform itemA, Transform itemB){
-		int weightA = itemA.GetComponent<CellScript> ().Weight;
-		int weightB = itemB.GetComponent<CellScript> ().Weight;
+	int SortAdjacentsByWeight(CellScript.CellNode itemA, CellScript.CellNode itemB)
+	{
+		int weightA = itemA.cell.GetComponent<CellScript> ().Weight;
+		int weightB = itemB.cell.GetComponent<CellScript> ().Weight;
 		return weightA.CompareTo (weightB);
 	}
 
-	void Update(){
+	void Update()
+	{
 		if(Input.GetKeyDown(KeyCode.R))
 		   Application.LoadLevel(0);
 	}
