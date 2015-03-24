9c45e650636965d52243fb2ff8039757a18abb32
Pranabesh <pranabesh.sinha@gmail.com>
2015-03-23 21:08:44 -0700

    Adding texture and AntagonistAlone script

---
+++ Assets/Scripts/AntagonistAlone.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntagonistAlone : MonoBehaviour {
+			
+	private Rigidbody rb;
+	public float speed;
+	
+	// All physics code goes here
+	void FixedUpdate () {
+		float moveHori = Input.GetAxis ("Horizontal");
+		float moveVert = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHori, -100.0f, moveVert);
+		
+		rb.AddForce (movement * speed * Time.deltaTime);
+		
+	}
+	
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+
+			
+}
+		
+	
