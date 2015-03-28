b503ed50e5d1d4d3b29635c7fb8f8e51e6a08c45
unknown <psinha4@psinha4-MOBL2.amr.corp.intel.com>
2015-03-28 15:42:36 -0700

    Antagonist can seek target by line of sight

--- Assets/Scripts/AntagonistAlone.cs
+++ Assets/Scripts/AntagonistAlone.cs
@@ -10,6 +10,7 @@ public class AntagonistAlone : MonoBehaviour {
 
 	float m_speed;
 	float m_speed_multi = 1.5f;
+	static float yPosn = 0f;
 
 	List<MazeNode> Maze;
 	Dictionary<Vector3, MazeNode> MazeMap;
@@ -19,36 +20,91 @@ public class AntagonistAlone : MonoBehaviour {
 	MazeNode currentNode;
 	MazeNode currentGoal;
 	public GameObject grid;
-	Vector3 origin = new Vector3(0f ,0f ,0f);
+	Vector3 forward  = new Vector3(1f ,yPosn ,0f);
+	Vector3 backward = new Vector3(-1f,yPosn ,0f);
+	Vector3 left     = new Vector3(0f ,yPosn ,1f);
+	Vector3 right    = new Vector3(0f ,yPosn ,-1f);
+	List<Ray> fourSides;
+	RaycastHit hit;
 
-
-	
 	// All physics code goes here
 	void FixedUpdate () {
 		float moveHori = Input.GetAxis ("Horizontal");
 		float moveVert = Input.GetAxis ("Vertical");
-		
-		Vector3 movement = new Vector3 (moveHori, -100.0f, moveVert);
-		
+		Vector3 movement = new Vector3 (moveHori, 0f, moveVert);
 		rb.AddForce (movement * speed * Time.deltaTime);
-
 	}
 	
 	void Start() {
 		rb = GetComponent<Rigidbody>();
 		Maze = grid.GetComponent<GridScript> ().Maze;
 		MazeMap = grid.GetComponent<GridScript> ().MazeMap;
+		whereIam = transform.position;
+		fourSides = new List<Ray> ();
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		whereINeedToBe = other.transform.position;
-	//	intermediatePosn = whereIam;
-	//	Debug.Log (whereIam);
 	}
 
-	//Update function which runs BFS
-	void Update()
+	void Update(){
+		Vector3 targetLoc;
+		bool found = false;
+		SeekTarget (out targetLoc, out found);
+		if (found) {
+		//	Debug.Log(targetLoc);
+			GetToLastSeenTargetPosn(targetLoc);
+		}
+	}
+
+	void GetToLastSeenTargetPosn(Vector3 target)
+	{
+		m_speed = Time.deltaTime * m_speed_multi;
+		whereIam = transform.position;
+		
+		Vector3 diffVector = target - whereIam;
+		//TODO: Fix this annoying bug
+		diffVector.y = 0;
+	
+		whereIam += diffVector * m_speed;
+		transform.position = whereIam;
+		
+	}
+
+	void SeekTarget(out Vector3 location, out bool found){
+		whereIam = transform.position;
+		location = Vector3.up;
+		found    = false;
+		//TODO:Fix accuracy problem
+		Ray rayFwd   = new Ray (whereIam, forward);
+		Ray rayBwd   = new Ray (whereIam, backward);
+		Ray rayLeft  = new Ray (whereIam, left);
+		Ray rayRight = new Ray (whereIam, right);
+		fourSides.Clear ();
+		fourSides.Add (rayFwd);
+		fourSides.Add (rayBwd);
+		fourSides.Add (rayLeft);
+		fourSides.Add (rayRight);
+		foreach (Ray rayDir in fourSides) {
+			Debug.DrawRay(rayDir.origin,rayDir.direction);
+			//Debug.Log(rayDir);
+			if(Physics.Raycast(rayDir, out hit)){
+				Debug.Log(hit.collider.tag);
+				if(hit.collider.tag == "Target"){
+				//	Debug.Log("Hit");
+				//	Debug.Log(rayDir);
+					location = hit.collider.transform.position;
+					found = true;
+					break;
+				}
+			}
+		}
+	}
+
+
+	//Uses BFS to get back to the exit
+	void GetToExit()
 	{
 		m_speed = Time.deltaTime * m_speed_multi;
 		whereIam = transform.position;
@@ -63,10 +119,6 @@ public class AntagonistAlone : MonoBehaviour {
 			Vector3 diffVector = currentGoal.thisEdge.position - whereIam;
 			//TODO: Fix this annoying bug
 			diffVector.y = 0;
-			/*float distanceLeft = Vector3.Distance(diffVector, origin);
-			if(distanceLeft <= 0.1)
-			{
-			}*/
 			whereIam += diffVector * m_speed;
 			transform.position = whereIam;
 		}
