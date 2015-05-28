26260dd5eae2e87e54b3f0ac1fc6ba9250f2517e
Pranabesh <pranabesh.sinha@gmail.com>
2015-05-28 09:17:07 -0700

    Trouble after reacing last seen lo

--- Assets/Scripts/AntagonistAlone.cs
+++ Assets/Scripts/AntagonistAlone.cs
@@ -23,7 +23,7 @@ public class AntagonistAlone : MonoBehaviour {
 	Vector3 right    = new Vector3(0f ,yPosn ,-1f);
 	List<Ray> fourSides;
 	RaycastHit hit;
-	enum AntagonistStates {IDLE, SEEN, RETURN, DEEP_SEARCH};
+	enum AntagonistStates {IDLE, SEEN, RETURN, DEEP_SEARCH, WALK};
 	AntagonistStates state = AntagonistStates.IDLE;
 	Vector3 targetSeenAt = Vector3.up;
 	public GameObject playerObj;
@@ -33,10 +33,10 @@ public class AntagonistAlone : MonoBehaviour {
 	float timeWhenWentIdle;
 
 	//DFS data structures
-	Transform[,] Grid;
-	bool startDFS;
-	MazeNode dfsStartPoint;
 	Stack<MazeNode> dfsStack;
+
+	//delete this
+	bool dfsOneTime = true;
 
 	void Start() {
 		Maze = grid.GetComponent<GridScript> ().Maze;
@@ -45,23 +45,12 @@ public class AntagonistAlone : MonoBehaviour {
 		fourSides = new List<Ray> ();
 		player = playerObj.GetComponent<Player> ();
 		lastTwoNodes = new Queue<Vector3> (2);
-		//delete this
-		Grid = grid.GetComponent<GridScript> ().Grid;
-		Transform start = Grid [0, 0];
 		dfsStack = new Stack<MazeNode> ();
-
-		MazeNode startNode = new MazeNode (start, 0, null);//no parent for start cell
-		if (MazeMap.TryGetValue (startNode.thisEdge.position, out currentNode)) {
-			dfsStartPoint = currentNode;
-		}
-		startDFS = true;
-		StartCoroutine	(DFSIterative (dfsStartPoint));
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		whereINeedToBe = other.transform.position;
-		other.transform.GetComponent<Renderer>().material.color = Color.red;
 	}
 
 	//Uses BFS to get back to the exit
@@ -83,28 +72,45 @@ public class AntagonistAlone : MonoBehaviour {
 		}
 	}
 
-	void GetToThisPosn(Vector3 target)
+	IEnumerator GetToThisPosn(Vector3 target)
 	{
 		target.y = 0.4f;
-		transform.position = Vector3.MoveTowards (transform.position, target, 0.04f * Time.deltaTime);
+		while (target != transform.position) 
+		{
+			transform.position = Vector3.MoveTowards (transform.position, target, 0.04f * Time.deltaTime);
+			yield return null;
+		}
+		state = AntagonistStates.DEEP_SEARCH;
 	}
 
-	IEnumerator DFSIterative(MazeNode currentNode)
+	IEnumerator DFSIterative(Vector3 startPos)
 	{
+		MazeMap.TryGetValue (startPos, out currentNode);
+		print ("CurrNode " + currentNode.thisEdge.position);
+		float startTime = Time.unscaledTime;
 		currentNode.dfsParent = null;
 		dfsStack.Push (currentNode);
 		MazeNode lastItemPopped = null;
 		float speed = 3.0f;
+		float searchFor = 6.0f;
 		while (dfsStack.Count != 0) 
 		{
+			if((Time.unscaledTime - startTime) > searchFor)
+			{
+				dfsStack.Clear();
+				break;
+			}
 			MazeNode node = dfsStack.Pop();
+			print ("Popped " + node.thisEdge.position);
 			if (node.dfsParent != null)
 			{
+				print ("Finally Get to parent at " + node.dfsParent.thisEdge.position);
 				while(whereINeedToBe != node.dfsParent.thisEdge.position) 
 				{
 					//try to get to the last item popped
 					Vector3 parent = lastItemPopped.thisEdge.position;
 					//if I arrive at the lastItemPopped or am there right now
+					print ("For now get to parent at " + parent);
 					if(whereINeedToBe.x == lastItemPopped.thisEdge.position.x && 
 					   whereINeedToBe.z == lastItemPopped.thisEdge.position.z)
 					{
@@ -118,6 +124,13 @@ public class AntagonistAlone : MonoBehaviour {
 					parent.y =  0.4f;
 					while(transform.position != parent)
 					{
+						print ("Blocking to get to parent " + parent);
+						print ("Currently at " + transform.position);
+						print ("Rounded off " + whereINeedToBe);
+//						if(transform.position != parent)
+//							print ("NEQ");
+//						else
+//							print ("isEQ");
 						transform.position = Vector3.MoveTowards (transform.position, parent, speed * Time.deltaTime);
 						yield return null;
 					}
@@ -128,14 +141,24 @@ public class AntagonistAlone : MonoBehaviour {
 			
 			Vector3 target = node.thisEdge.position;
 			target.y =  0.4f;
+			print ("Go to popped item " + target);
 			while(transform.position != target)
 			{
+				print ("Blocking to get to target " + target);
+				print ("Currently at " + transform.position);
+				print ("Rounded off " + whereINeedToBe);
+//				if(transform.position.x != target.x &&
+//				   transform.position.z != target.z)
+//					print ("NEQ");
+//				else
+//					print ("isEQ");
 				transform.position = Vector3.MoveTowards (transform.position, target, speed * Time.deltaTime);
 				yield return null;
 			}
 		    /// End of block to get to node
 			foreach(MazeNode adjacentNode in node.Adjacents)
 			{
+				print ("Pushed " + adjacentNode.thisEdge.transform);
 				adjacentNode.dfsParent = node;
 				dfsStack.Push(adjacentNode);
 			}
@@ -147,9 +170,8 @@ public class AntagonistAlone : MonoBehaviour {
 		if (MazeMap.TryGetValue (whereINeedToBe, out currentNode)) {
 			while(currentNode.bfsParent != null)
 			{
-				currentGoal = currentNode.bfsParent;
-				currentNode = currentGoal;
-				Vector3 goal = currentGoal.thisEdge.position;
+				currentNode = currentNode.bfsParent;
+				Vector3 goal = currentNode.thisEdge.position;
 				goal.y = 0.4f;
 				while(transform.position != goal)
 				{
@@ -196,70 +218,46 @@ public class AntagonistAlone : MonoBehaviour {
 	}
 
 	void Update(){
-		m_speed = Time.deltaTime * m_speed_multi;
-		
+		bool found = false;
 		//targetSeenAt should round off to the Cell's collison boundary placed in the centre
-		//		SeekTarget (out targetSeenAt, out found);
-		////		Debug.Log (targetSeenAt);
-		//
-		//		//the player could have run away in which case UP will be returned
-		//		if (!targetSeenAt.Equals (Vector3.up)) {
-		//			lastSeenAt = targetSeenAt;
-		//		} 
-		//
-		//		if (found) {
-		//			state = AntagonistStates.SEEN;
-		//			wentToIdle = false;
-		//		}
-		//		else if (transform.position.Equals (lastSeenAt)) {
-		//			state = AntagonistStates.IDLE;
-		//			if(!wentToIdle){
-		//				wentToIdle = true;
-		//				timeWhenWentIdle = Time.unscaledTime;
-		//			}
-		//		}
-		//		if (wentToIdle) {
-		//			//perform DFS for 10s
-		//			if((Time.unscaledTime - timeWhenWentIdle) > 10){
-		//				state = AntagonistStates.RETURN;
-		//				wentToIdle = false;
-		//			}
-		//			else{
-		//				//trigger dfs here
-		//				state = AntagonistStates.DEEP_SEARCH;
-		//			}
-		//		}
-		//
-		//		switch (state) {
-		//			case AntagonistStates.SEEN:{
-		//				transform.GetComponent<Renderer>().material.color = Color.red;
-		//				GetToThisPosn(lastSeenAt);
-		//				break;
-		//			}
-		//			case AntagonistStates.IDLE:
-		//			{
-		//				transform.GetComponent<Renderer>().material.color = Color.white;
-		//				break;
-		//			}
-		//			case AntagonistStates.RETURN:
-		//			{
-		//				transform.GetComponent<Renderer>().material.color = Color.white;
-		//				GetToExit();
-		//				break;
-		//			}
-		//			case AntagonistStates.DEEP_SEARCH:{
-		//				transform.GetComponent<Renderer>().material.color = Color.green;
-		//				DFSInit();
-		//				break;
-		//			}
-		//			default:
-		//			{
-		//				transform.GetComponent<Renderer>().material.color = Color.yellow;
-		//				break;
-		//			}
-		//		}
-
-		
+		SeekTarget (out targetSeenAt, out found);
+		if (found) {
+			state = AntagonistStates.SEEN;
+			lastSeenAt = targetSeenAt;
+		}
+		switch (state) {
+			case AntagonistStates.SEEN:{
+				transform.GetComponent<Renderer>().material.color = Color.red;
+				StartCoroutine	(GetToThisPosn(lastSeenAt));
+				state = AntagonistStates.WALK;
+				break;
+			}
+			case AntagonistStates.WALK:
+			{
+				transform.GetComponent<Renderer>().material.color = Color.yellow;
+				break;
+			}
+//			case AntagonistStates.RETURN:
+//			{
+//				transform.GetComponent<Renderer>().material.color = Color.yellow;
+//				GetToExit();
+//				break;
+//			}
+			case AntagonistStates.DEEP_SEARCH:{
+				transform.GetComponent<Renderer>().material.color = Color.green;
+				//delete this
+				lastSeenAt.y = 0.0f;
+				print ("Deep searching from "+lastSeenAt);
+				StartCoroutine	(DFSIterative (lastSeenAt));
+				state = AntagonistStates.WALK;
+				break;
+			}
+			default:
+			{
+				transform.GetComponent<Renderer>().material.color = Color.white;
+				break;
+			}
+		}
 	}
 }
 		
